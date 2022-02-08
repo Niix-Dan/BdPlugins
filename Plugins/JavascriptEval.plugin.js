5b4936524c1426914b0f34471901e43c8426774f
Daniel <46878004+Daniel-code15@users.noreply.github.com>
2022-02-08 16:05:26 -0300

    Add files via upload

--- Plugins/JavascriptEval.plugin.js
+++ Plugins/JavascriptEval.plugin.js
@@ -5,7 +5,7 @@
 */
 
 
-let t;
+let t, b;
 const config = {
     info: {
         name: "JavascriptEval",
@@ -27,26 +27,26 @@ module.exports = class {
     getVersion(){ return config.info.version }
 
 
-    start() { 
+    start() {
         this.clearButtons();
         this.registerButtons() 
     }
     load() { 
-        this.clearButtons();
-        this.registerButtons() 
     }
     stop() {
         this.clearButtons();
     }
+    onSwitch() {
+        this.clearButtons();
+        this.registerButtons();
+    }
 
     clearButtons() {
-        let elements = document.getElementsByClassName("EvalExecutor"); 
-        for(let i = 0 ; i < elements.length ; i++) {
-            let el = elements[i];
-            
-            el.remove()
+        if(b) {
+            b.remove(); 
+            b = null;
         }
-    } 
+    }
 
 
     Click() {
@@ -75,54 +75,36 @@ module.exports = class {
     }
 
     registerButtons() {
-        let divs = document.querySelectorAll("div");
-        let classe;
-
-        for(let i = 0 ; i < divs.length ; i++) {
-            let div = divs[i];
-            let pclass = `${div.classList[0]}`;
-            
-            if(pclass.match(/buttons-(([^a-zA-Z]*[A-Za-z]){4})-[0-9]/g)) {
-                classe = div;
-            }
-        }
-
-        if(!classe || !classe.children[0] || !classe.children[0].children) return;
-
-        let button = document.createElement("button");
-        let middle = document.createElement("div");
-        let last = document.createElement("div");
-        
-        let id_class = classe.children[0].classList;
-
-        middle.classList = classe.children[0].children[0].classList;
-        last.classList = classe.children[0].children[0].children[0].classList;
-
-        button.onclick = this.Click;
-        button.type = "button"
-        button.classList = id_class;
-
-        let path = `<path fill="currentColor" fill-rule="evenodd" clip-rule="evenodd" d="M25.323,1.948H1.949C0.871,1.948,0,2.816,0,3.894v19.481c0,1.074,0.871,1.946,1.949,1.946h23.374
+        const ChannelTextAreaButtons = BdApi.findModule(m => m.type && m.type.displayName === "ChannelTextAreaButtons")
+        const ChannelTextAreaButton = BdApi.findModule(m => m.type?.displayName === "ChannelTextAreaButton");
+        let d = `M25.323,1.948H1.949C0.871,1.948,0,2.816,0,3.894v19.481c0,1.074,0.871,1.946,1.949,1.946h23.374
         c1.071,0,1.946-0.872,1.946-1.946V3.894C27.269,2.816,26.395,1.948,25.323,1.948z M9.315,3.411c0.536,0,0.971,0.431,0.971,0.973
         c0,0.537-0.435,0.973-0.971,0.973c-0.538,0-0.978-0.436-0.978-0.973C8.337,3.842,8.777,3.411,9.315,3.411z M6.333,3.411
         c0.535,0,0.973,0.431,0.973,0.973c0,0.537-0.438,0.973-0.973,0.973c-0.541,0-0.977-0.436-0.977-0.973
         C5.356,3.842,5.792,3.411,6.333,3.411z M3.408,3.411c0.539,0,0.974,0.431,0.974,0.973c0,0.537-0.435,0.973-0.974,0.973
-        c-0.536,0-0.972-0.436-0.972-0.973C2.436,3.842,2.872,3.411,3.408,3.411z M25.323,23.376H1.949V6.839h23.374V23.376z"></path>`
+        c-0.536,0-0.972-0.436-0.972-0.973C2.436,3.842,2.872,3.411,3.408,3.411z M25.323,23.376H1.949V6.839h23.374V23.376z`
 
-        last.innerHTML = `
-<svg width="24" height="24" class="icon-1d5XAh" aria-hidden="false" viewBox="0 0 28 28">${path}</svg>
-        `
-        button.appendChild(middle);
-        middle.appendChild(last);
-        classe.appendChild(button);
+        try {
+            BdApi.Patcher.after(config.info.name, ChannelTextAreaButtons, "type", (_, [props], ret) => {
+                try {
+                    let path = BdApi.React.createElement("path", { 
+                        fill: "currentColor", 
+                        "fill-rule": "evenodd", 
+                        "clip-rule": "evenodd", 
+                        d 
+                    });
+                    let svg = BdApi.React.createElement("svg", {width:24, height:24, viewBox:"0 0 28 28"}, path)
+                    let button = BdApi.React.createElement("div", {style: {minWidth: "50px"}},svg);
+                    let buttonContainer = BdApi.React.createElement(ChannelTextAreaButton, { onClick: this.Click }, button);
 
-        button.classList.add("EvalExecutor");
-        middle.classList.add("EvalExecutor");
-        last.classList.add("EvalExecutor");
-    }
-
-    onSwitch() {
-        this.clearButtons();
-        this.registerButtons();
+                    b = buttonContainer;
+                    ret.props.children.push(buttonContainer)
+                } catch(err) {
+                    BdApi.showToast(err, {type: "error"});
+                }
+            })
+        } catch(err) {
+            BdApi.showToast(err, {type: "error"});
+        }
     }
 }
